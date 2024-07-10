7f77ed2b452a79025e44d3e0769490e754905867
Abel Mitiku <abelmgetnet@gmail.com>
2024-07-10 14:42:08 +0300

    d

--- backend/app/app.py
+++ backend/app/app.py
@@ -24,7 +24,7 @@ run_banner()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
