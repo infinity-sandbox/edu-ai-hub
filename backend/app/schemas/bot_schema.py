db15552be7e9a52727cabafdbf550a65a6a9aed7
Abel Mitiku <abelmgetnet@gmail.com>
2024-07-27 02:48:18 +0300

    add intro text to speech and formated question generator

--- backend/app/schemas/bot_schema.py
+++ backend/app/schemas/bot_schema.py
@@ -4,7 +4,11 @@ from pydantic import BaseModel
 from typing import List
 import io
 
-# Data model for JSON response
+# Data model for subject selection input
+class SubjectSelection(BaseModel):
+    selectedClass: str
+
+# Data model for json mouth cues response
 class MouthCue(BaseModel):
     start: float
     end: float
@@ -17,6 +21,12 @@ class Metadata(BaseModel):
 class JSONResponseModel(BaseModel):
     metadata: Metadata
     mouthCues: List[MouthCue]
+
+# Display text on the blackboard
+class DisplayText(BaseModel):
+    text: str
     
-class SubjectSelection(BaseModel):
-    selectedClass: str
+class BotFirstResponse(BaseModel):
+    audio_url: str
+    json_data: JSONResponseModel
+    question: str
