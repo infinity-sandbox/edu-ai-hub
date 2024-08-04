1b4c73d7553873b56c5cb27d7419fa0b8a2bd4cf
Abel Mitiku <abelmgetnet@gmail.com>
2024-08-04 22:27:40 +0300

    update

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
   const isAuthenticated = !!localStorage.getItem('token'); // Check if token exists in local storage
-  return isAuthenticated ? children : <Navigate to="/Login" />;
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
